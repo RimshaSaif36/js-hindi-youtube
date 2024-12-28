daa46ef45fd7bfdcae8826582a4761500088ed17
RimshaSaif36 <Khokharrimsha723@gmail.com>
2024-12-28 14:15:02 +0000

    datatypes

---
+++ 01_basics/01_varaibles.js
@@ -0,0 +1,16 @@
+const accountId =123
+let accountEmail="rimsha@gmail.com"
+var accountPassword="345"
+accountCity="gujranwala"
+// accountId= 789 // not allowed
+
+accountEmail="ry@gmail.com"
+accountPassword="35"
+accountCity="lahore"
+let accountState;
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+/*
+Prefer not use var 
+because of issue in block space and functional scope 
+*/
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strick"; //treat all code as newer version 
+
+//alert(3 + 3)//we are using nodejs,not browser
+
+console.log(3+3)
+
+let name="rimsha"
+let age =18
+let isLoggedIn =false
+let state;
+
+//number => 2 to power 53
+//biginit
+//string =>""
+//boolean => true/false
+//null=>standalone value // null is a object
+//undefined=>
+    //symbol=>unique
+//object
+
+console.log(typeof age);
