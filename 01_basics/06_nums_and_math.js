c92eb00d4606359eda6c710ba005138a6cd4e7f0
RimshaSaif36 <Khokharrimsha723@gmail.com>
2024-12-29 14:21:50 +0000

    math and number

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,36 @@
+// const score= 400
+// console.log(score)
+// const balance =new Number(100)
+// console.log(balance)
+
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(3))
+// console.log(balance.toString())
+
+// const otherNumber=45.34545
+// console.log(otherNumber.toPrecision(3))
+
+// const hundreds= 100000000
+// console.log(hundreds.toLocaleString('en-PK'))
+// console.log(hundreds.toLocaleString('en-ID'))
+
+//*********************Math************
+// console.log(Math)// Object
+// console.log(Math.abs(4.2))
+// console.log(Math.round(4.5))
+// console.log(Math.floor(4.9))
+// console.log(Math.ceil(4.5))
+// console.log(Math)// Object
+// console.log(Math.abs(4.2))
+// console.log(Math.round(4.5))
+// console.log(Math.floor(4.9))
+
+// console.log(Math.min(4.5,4,7,1,34))
+// console.log(Math.max(4.5,4,7,1,34))
+
+// console.log(Math.random())
+// console.log(Math.random()*10 +1)
+
+const min =10
+const max =20
+console.log(Math.floor(Math.random()* (max-min +1))+min)
