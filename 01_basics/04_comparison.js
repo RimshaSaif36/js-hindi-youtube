495db2a4ae8bc47c725012e81351d262b603dd94
RimshaSaif36 <Khokharrimsha723@gmail.com>
2024-12-29 10:46:46 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,21 @@
+// console.log(2>1)
+// console.log(2>=1)
+// console.log(2<1)
+// console.log(2==1)
+// console.log(2!=1)
+// console.log("2">1)
+
+ //  we avoid these type of code
+
+// console.log(null==0)
+// console.log(null>0)
+// console.log(null>=0)
+
+// console.log(undefined==0)
+// console.log(undefined>0)
+// console.log(undefined < 0)
+
+
+// strick check (not only check values ,check datatypes)  ===
+
+console.log("1"==="true")
