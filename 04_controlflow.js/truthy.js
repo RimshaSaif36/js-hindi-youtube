e489ac3e657e98fc1f48285c5aed2dc334415bbf
RimshaSaif36 <Khokharrimsha723@gmail.com>
2025-01-03 15:54:41 +0000

    truthy

---
+++ 04_controlflow.js/truthy.js
@@ -0,0 +1,24 @@
+// const userEmail = []
+// if ( userEmail ) {
+//     console.log("got mail");
+// }
+// else{
+//     console.log("dont got mail")
+// }
+
+//falsy values
+
+// false, 0,-0,bigInt, 0n, ,null,undefined,NaN
+
+//Trythy value
+
+// "0","false","",[],{},function(){}
+
+// if(userEmail.length  === 0){
+// console.log("array is empty")
+// }
+
+const emptyobj= {}
+if (Object.keys(emptyobj).length === 0){
+console.log("empty")
+}
